9a31016af3e42779f3d86306cca4c207d983e519
SWATHI-R412 <125952006+SWATHI-R412@users.noreply.github.com>
2024-01-22 14:52:38 +0530

    Add files via upload

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,12 @@
+import Demo_Page from "./components/Demo_Page"
+
+function App() {
+  
+
+  return (
+    
+    <Demo_Page/>
+  )
+}
+
+export default App
---
+++ FRONTEND/src/components/Demo_Page.jsx
@@ -0,0 +1,6 @@
+function Demo_Page(){
+    return(
+        <h1>First day of advanced app development...</h1>
+    )
+}
+export default Demo_Page
